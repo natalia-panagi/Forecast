54a952ba49cabb8b3f44e29810ed554ba77e77d9
natalia-panagi <natalia.panagi@aol.com>
2026-03-25 10:39:02 +0200

    css changes

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function refreshWeather(response){
        let iconElement = document.querySelector("#icon");
        iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class=current-temperature-icon/>`;
 
-
+    getForecast(response.data.city);
    }
 
 
@@ -63,24 +63,41 @@ function handleSearchSubmit(event){
 }
 searchCity("Madrid");
 
-function displayForecast() {
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[date.getDay()];
+
+}
+
+function getForecast(city){
+let apiKey = "fb6a7o33d928a9e256t114bb04022298";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+axios(apiUrl).then(displayForecast);
+
+}
+
+
+function displayForecast(response) {
+    console.log(response.data);
 
 let forecastElement = document.querySelector("#forecast");
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
 let forecastHtml = "";
 
-days.forEach(function(day) {
-forecastHtml = forecastHtml + `          
+response.data.daily.forEach(function(day, index) { if (index < 5){
+forecastHtml =
+  forecastHtml +
+  `          
         <div class="forecast-day">
-            <div class="forecast-weekday">${day}</div>
-            <div class="forecast-emoji">☀️</div>
+            <div class="forecast-weekday">${formatDay(day.time)}</div>
+            <div class="forecast-emoji"> <img src = "${day.condition.icon_url}" class = "forecast-image"> </div>
             <div class="forecast-temperatures">
-                <div class="forecast-temperature-high"> 15°C </div>
-                <div class="forecast-temperature-low"> 10°C</div>
+                <div class="forecast-temperature-high"> ${Math.round(day.temperature.maximum)}°C </div>
+                <div class="forecast-temperature-low"> ${Math.round(day.temperature.minimum)}°C</div>
             </div>
         </div>
         `;
-       
+} 
 });
  forecastElement.innerHTML = forecastHtml;
 }
@@ -89,6 +106,6 @@ forecastHtml = forecastHtml + `
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-displayForecast();
+
 
 
